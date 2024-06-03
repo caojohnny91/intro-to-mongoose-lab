22ca6fe1f2584196652129d75c3e2cfdf70a4b66
jcao91 <jcao91@yahoo.com>
2024-06-02 22:37:45 -0500

    console.log outputs created

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
+// load the customer model
 const Customer = require("./models/customer.js");
 
-const username = prompt("What is your name? ");
 
-console.log(`Your name is ${username}`);
+function startApp() {
+    console.log('Welcome to the CRM (Customer Relationship Magagement');
+    console.log('What would you like to do?');
+    console.log('   1. Create a customer');
+    console.log('   2. View all customers');
+    console.log('   3. Update a customer');
+    console.log('   4. Delete a customer');
+    console.log('   5. Quit');
+}
